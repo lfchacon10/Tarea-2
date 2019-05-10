c545bd937def76fc9b7c1e0585f93033b3743265
Fernando <luisfer_chacon@hotmail.com>
2019-05-10 06:11:34 -0500

    Inicializados puntos RK

--- edificio.cpp
+++ edificio.cpp
@@ -16,11 +16,11 @@ float  forzExterno (float w, float t)
 {
   return sin(w*t*PI/180);
 }
-float ecuacion1( float u1, float u1, float v1, float v2, float w,)
+float ecuacion1( float u1, float u2, float v1, float w, float t)
 {
   return -(fric/m)*v1 - 2*(k/m)*u1 + (k/m)*u2 + forzamiento(w,t)/m;
 }
-float ecuacion2( float u1, float u1, float v1, float v2, float w,)
+float ecuacion2( float u1, float u2, float u3, float v2)
 {
   return -(fric/m)*v2 + (k/m)*u1 - 2*(k/m)*u2 + (k/m)*u3;
 }
@@ -29,25 +29,41 @@ float ecuacion3( float u2, float u3, float v3)
   return -(fric/m)*v3 + (k/m)*u2 - (k/m)*u3;
 }
 
-float lf ( float ang, int pun)
+float lf ( float u, int v)
 {
   //Se iniciliazan los tres primeros puntos
   float puntos[3][3];
 
 }
 
-
-float rk ( float ang, int pun)
+float rk ( float u1, float u2, float u3,float v1, float v2, float v3, float t, float w  )
 {
-  //Se iniciliazan los tres primeros puntos
-  float puntos[3][3];
+  //Se iniciliazan las posiciones de acuerdo a las velocidades en t=0
+
+  float puntos[4][6];
+  puntos[0][0]= dt*v1;
+  puntos[0][1]= dt*v2;
+  puntos[0][2]= dt*v3;
+  //Posiciones futuras
+  puntos[0][3]= dt*ecuacion1( u1,  u2,  v1,  w,  t);
+  puntos[0][4]= dt*ecuacion2( u1,  u2,  u3,  v2);
+  puntos[0][5]= dt*ecuacion3( u2,  u3,  v3);
+
 
 }
 int main()
 {
+  //Para LeapFrog
   float u=0;
   float v=0;
   float x0=lf(u, v);
+
+
+  //Para RungeKuta
+  float u1 = 0.0, u2 = 0.0, u3 = 0.0;
+  float v1 = 0.0, v2 = 0.0, v3 = 0.0;
+
+  float x0=rk(u, v);
 
 	return 0;
 }
