8583236277a703c28eab6d0b092174456631dd76
Fernando <luisfer_chacon@hotmail.com>
2019-05-11 17:35:41 -0500

    Funciona con valor edificio. Se puede meter los valores que uno quieran

--- hw2.mk
+++ hw2.mk
@@ -1,5 +1,5 @@
-Resultados_hw2.pdf: Senal.png TransformadasSenales.png amplitud1.png amplitud2.png amplitud3.png amplitud4.png
-
+Resultados_hw2.pdf: Senal.png TransformadasSenales.png EspectrogramaSenales.png EspectrogramaTemblor.png
+	pdflatex Resultados_hw2.tex
 
 Senal.png: Fourier.py
 	python Fourier.py
@@ -7,19 +7,30 @@ Senal.png: Fourier.py
 TransformadasSenales.png:Fourier.py
 	python Fourier.py
 
+EspectrogramaSenales.png:Fourier.py
+	python Fourier.py
+
+SenalSismica.png:Fourier.py
+	python Fourier.py
+
+TransformadaFourierSenalSismica.png:Fourier.py
+	python Fourier.py
+
+EspectrogramaTemblor.png:Fourier.py
+	python Fourier.py
+
 amplitud1.png:Plotws_hw2.py amp.dat
 	python Plotws_hw2.py
 
-amplitud2.png:Plotws_hw2.py amp.dat
-	python Plotws_hw2.py
+#amplitud2.png:Plotws_hw2.py amp.dat
+#	python Plotws_hw2.py
 
-amplitud3.png:Plotws_hw2.py amp.dat
-	python Plotws_hw2.py
+#amplitud3.png:Plotws_hw2.py amp.dat
+#	python Plotws_hw2.py
 
-amplitud4.png:Plotws_hw2.py amp.dat
-	python Plotws_hw2.py
-
+#amplitud4.png:Plotws_hw2.py amp.dat
+#	python Plotws_hw2.py
 
 amp.dat: Edificio.cpp
 	g++ edificio.cpp
-	./a.out
+	./a.out 2.0
