8a796186e17fa8a4bb4377227b6eb9c74de7996d
Fernando <luisfer_chacon@hotmail.com>
2019-05-11 19:43:48 -0500

    mkfile Actualizado con las nuevas imagenes y lo del pdf

--- hw2.mk
+++ hw2.mk
@@ -1,4 +1,4 @@
-Resultados_hw2.pdf: Senal.png TransformadasSenales.png EspectrogramaSenales.png EspectrogramaTemblor.png
+Resultados_hw2.pdf: Senal.png TransformadasSenales.png EspectrogramaSenales.png EspectrogramaTemblor.png amplitud1.png amplitudesMax.png
 	pdflatex Resultados_hw2.tex
 
 Senal.png: Fourier.py
@@ -22,15 +22,12 @@ EspectrogramaTemblor.png:Fourier.py
 amplitud1.png:Plotws_hw2.py amp.dat
 	python Plotws_hw2.py
 
-#amplitud2.png:Plotws_hw2.py amp.dat
-#	python Plotws_hw2.py
-
-#amplitud3.png:Plotws_hw2.py amp.dat
-#	python Plotws_hw2.py
-
-#amplitud4.png:Plotws_hw2.py amp.dat
-#	python Plotws_hw2.py
-
-amp.dat: Edificio.cpp
+amp.dat: edificio.cpp
 	g++ edificio.cpp
-	./a.out 2.0
+	./a.out
+
+amplitud1.png:Plotws_hw2.py amp.dat
+	python Plotws_hw2.py
+
+amplitudesMax.png:Plotws_hw2.py amp.dat
+	python Plotws_hw2.py
