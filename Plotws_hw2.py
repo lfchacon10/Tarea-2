aefe71a41ac97f4324619ec58dfe7b1a9627b46a
Fernando <luisfer_chacon@hotmail.com>
2019-05-11 20:34:56 -0500

    Pone las cuatro frecuencias elegidas

--- Plotws_hw2.py
+++ Plotws_hw2.py
@@ -1,27 +1,50 @@
 import matplotlib.pylab as plt
 import numpy as np
 
-#Para t=0
 datos = np.genfromtxt("amp.dat", delimiter=",")
 t= datos[:,0]
 u1= datos[:,1]
 u2= datos[:,2]
 u3= datos[:,3]
-#v1= datos[:,4]
-#v2= datos[:,5]
-#v3= datos[:,6]
 
 plt.figure()
 plt.grid()
-plt.title("Amplitudes")
-plt.plot(t[:1000],u1[:1000], label="Amplitud1")
-plt.plot(t[:1000],u2[:1000], label="Amplitud2")
-plt.plot(t[:1000],u3[:1000], label="Amplitud3")
-plt.ylabel("Amplitud")
+plt.title("Amplitudes para frecuencia de forzamiento = 1*w")
+plt.plot(t[28*1000:29*1000],u1[28*1000:29*1000], label="Amplitud1") #Entre 28 y 29 porque esos son los datos de 1*w
+plt.plot(t[28*1000:29*1000],u2[28*1000:29*1000], label="Amplitud2")
+plt.plot(t[28*1000:29*1000],u3[28*1000:29*1000], label="Amplitud3")
+plt.ylabel("Amplitudes")
+plt.xlabel("Tiempo(s)")
+plt.legend()
+plt.savefig("amplitudes.png")
+
+
+plt.figure()
+plt.grid()
+plt.title("Amplitud 1 para frecuencia de forzamiento = 1*w")
+plt.plot(t[28*1000:29*1000],u1[28*1000:29*1000], label="Amplitud1") #Entre 28 y 29 porque esos son los datos de 1*w
+plt.ylabel("Amplitudes frecuencia de forzamiento = 1*w")
 plt.xlabel("Tiempo(s)")
 plt.legend()
 plt.savefig("amplitud1.png")
-plt.show()
+
+plt.figure()
+plt.grid()
+plt.title("Amplitud 2 para frecuencia de forzamiento = 1*w")
+plt.plot(t[28*1000:29*1000],u2[28*1000:29*1000], "orange",label="Amplitud2") #Entre 28 y 29 porque esos son los datos de 1*w
+plt.ylabel("Amplitudes frecuencia de forzamiento = 1*w")
+plt.xlabel("Tiempo(s)")
+plt.legend()
+plt.savefig("amplitud2.png")
+
+plt.figure()
+plt.grid()
+plt.title("Amplitud 3 para frecuencia de forzamiento = 1*w")
+plt.plot(t[28*1000:29*1000],u3[28*1000:29*1000],"g", label="Amplitud3") #Entre 28 y 29 porque esos son los datos de 1*w
+plt.ylabel("Amplitudes frecuencia de forzamiento = 1*w")
+plt.xlabel("Tiempo(s)")
+plt.legend()
+plt.savefig("amplitud3.png")
 
 u1ampMax=[]
 u2ampMax=[]
@@ -51,4 +74,53 @@ plt.ylabel("Amplitud")
 plt.yscale("Log")
 plt.xlabel("Frecuencia de forzamiento")
 plt.legend()
+plt.savefig("amplitudesMax.png")
+
+
+plt.figure()
+plt.grid()
+plt.title("Amplitudes para frecuencia de forzamiento = 0.4*w")
+plt.plot(t[10*1000:11*1000],u1[10*1000:11*1000], label="Amplitud1") #Entre 28 y 29 porque esos son los datos de 1*w
+plt.plot(t[10*1000:11*1000],u2[10*1000:11*1000], label="Amplitud2")
+plt.plot(t[10*1000:11*1000],u3[10*1000:11*1000], label="Amplitud3")
+plt.ylabel("Amplitudes")
+plt.xlabel("Tiempo(s)")
+plt.legend()
+plt.savefig("amplitudes04.png")
+
+
+plt.figure()
+plt.grid()
+plt.title("Amplitudes para frecuencia de forzamiento = 1.2*w")
+plt.plot(t[39*1000:40*1000],u1[39*1000:40*1000], label="Amplitud1") #Entre 28 y 29 porque esos son los datos de 1*w
+plt.plot(t[39*1000:40*1000],u2[39*1000:40*1000], label="Amplitud2")
+plt.plot(t[39*1000:40*1000],u3[39*1000:40*1000], label="Amplitud3")
+plt.ylabel("Amplitudes")
+plt.xlabel("Tiempo(s)")
+plt.legend()
+plt.savefig("amplitudes12.png")
+
+
+plt.figure()
+plt.grid()
+plt.title("Amplitudes para frecuencia de forzamiento = 1.7*w")
+plt.plot(t[53*1000:54*1000],u1[53*1000:54*1000], label="Amplitud1") #Entre 28 y 29 porque esos son los datos de 1*w
+plt.plot(t[53*1000:54*1000],u2[53*1000:54*1000], label="Amplitud2")
+plt.plot(t[53*1000:54*1000],u3[53*1000:54*1000], label="Amplitud3")
+plt.ylabel("Amplitudes")
+plt.xlabel("Tiempo(s)")
+plt.legend()
+plt.savefig("amplitudes17.png")
+
+
+plt.figure()
+plt.grid()
+plt.title("Amplitudes para frecuencia de forzamiento = 3*w")
+plt.plot(t[99*1000:100*1000],u1[99*1000:100*1000], label="Amplitud1") #Entre 28 y 29 porque esos son los datos de 1*w
+plt.plot(t[99*1000:100*1000],u2[99*1000:100*1000], label="Amplitud2")
+plt.plot(t[99*1000:100*1000],u3[99*1000:100*1000], label="Amplitud3")
+plt.ylabel("Amplitudes")
+plt.xlabel("Tiempo(s)")
+plt.legend()
+plt.savefig("amplitudes30.png")
 plt.show()
